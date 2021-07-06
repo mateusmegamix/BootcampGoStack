97d864aa6f2a826dbb9380cdcd39da3d80936ac0
Mateus Pereira <mateus.pereira@am4.com.br>
2021-07-05 22:10:07 -0300

    Update aulas de banco de dados

--- iniciado-back-end/src/database/migrations/1625094471036-CreateAppointments.ts
+++ iniciado-back-end/src/database/migrations/1625094471036-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1625094471036 implements MigrationInterfa
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()',
@@ -16,13 +16,21 @@ export default class CreateAppointments1625094471036 implements MigrationInterfa
             {
               name: 'provider',
               type: 'varchar',
-              isNullable: false,
             },
             {
               name: 'date',
               type: 'timestamp with time zone',
-              isNullable: false,
-            }
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
           ]
         })
       );
