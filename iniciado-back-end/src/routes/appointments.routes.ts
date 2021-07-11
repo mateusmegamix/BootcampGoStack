17690ccbefac02b5e356ce0d14b18a5a8563e651
Mateus Pereira <mateus.pereira@am4.com.br>
2021-07-11 16:43:24 -0300

    bootcamp-update

--- iniciado-back-end/src/routes/appointments.routes.ts
+++ iniciado-back-end/src/routes/appointments.routes.ts
@@ -5,9 +5,15 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
 
+appointmentsRouter.use(ensureAuthenticated);
+
 appointmentsRouter.get('/', async (request, response) => {
+  //console.log(request.user);
+
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
@@ -15,7 +21,7 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
+
     const {provider_id, date} = request.body;
 
     const parseDate = parseISO(date);
@@ -28,9 +34,6 @@ appointmentsRouter.post('/', async (request, response) => {
     });
 
     return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({error: err.message});
-  }
-})
+});
 
 export default appointmentsRouter;
