0966594c8ee95d3c2fb40b3333d98bede58acbba
Mateus Pereira <mateus.pereira@am4.com.br>
2022-01-05 18:47:42 -0300

    correções

--- gobarber-web/src/components/ToastContainer/index.tsx
+++ gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import Toast from './Toast'
-import {Container} from './styles'
-import {ToastMessage} from '../../hooks/toast'
-import {useTransition} from 'react-spring'
+import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './Toast';
+
+import { ToastMessage } from '../../hooks/toast';
+import { Container } from './styles';
 
 interface ToastContainerProps {
-    messages: ToastMessage[];
+  messages: ToastMessage[];
 }
 
-const ToastContainer: React.FC<ToastContainerProps> = ({messages}) => {
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
 
-    const messagesWithTransitions = useTransition(
-        messages,
-        (message) => message.id,
-        {
-            from: { right: '-120%', opacity: 0 },
-            enter: { right: '0%', opacity: 1  },
-            leave: { right: '-120%', opacity: 0  }
-        },
-    );
-
-    return (
-        <Container>
-            {messagesWithTransitions.map(({ item, key, props }) => (
-                <Toast key={key} style={props} message={item}/>
-            ))}
-        </Container>
-    );
+  return (
+    <Container>
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
 };
 
-export default ToastContainer
+export default ToastContainer;
